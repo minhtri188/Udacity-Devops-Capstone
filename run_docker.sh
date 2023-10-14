efaeef1d1195a324b73afa30276ba3aa87847395
minhtri188 <102676218+minhtri188@users.noreply.github.com>
2023-10-14 21:59:08 +0700

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,12 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t captone-app-final .
+docker build --tag=devops-capstone .
 
 # Step 2:
 # List docker images
-docker images
+docker images list
 
 # Step 3:
 # Run flask app
-# docker run -p 8000:80 captone
+docker run -p 8000:80 devops-capstone
