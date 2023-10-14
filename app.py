b30579b9aa178e89f239c9e5ddc89e3f0eb4b935
minhtri188 <102676218+minhtri188@users.noreply.github.com>
2023-10-14 22:00:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-    return "Hello World! ThaoDV4"
+    return "Hello, My name is Tri"
 
 if __name__ == "__main__":
 
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True)
