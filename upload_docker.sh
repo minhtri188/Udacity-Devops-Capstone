efe395df74e17751b8e62e8acc6daa2cf6147272
minhtri188 <102676218+minhtri188@users.noreply.github.com>
2023-10-14 21:54:03 +0700

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,15 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-projectname=captone-app-final
-dockerpath=dovanthao2404/$projectname
-docker tag $projectname $dockerpath
+dockerpath=minhtri188/devops-capstone
 
 # Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login
+docker tag devops-capstone $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
-docker push $dockerpath
+docker push $dockerpath:latest
